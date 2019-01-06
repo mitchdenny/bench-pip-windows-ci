6efea132ce0c66770bacae7bc093693db6595a04
Mitch Denny <mitch@mitchdenny.com>
2019-01-05 21:30:04 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -15,11 +15,11 @@ if ($UseVenv) {
     myenv\Scripts\activate
 }
 
-python -m pip install -U pip --cache-dir $BUILD_SOURCESDIRECTORY\pipcache
+python -m pip install -U pip
 
 python --version
 python -c "import struct; print(struct.calcsize('P') * 8, 'bits')"
 pip --version
 
-Measure-Command {pip install -r requirements.txt --cache-dir $BUILD_SOURCESDIRECTORY\pipcache }
+Measure-Command {pip install -r requirements.txt }
 pip list
