de500bc77833ddcc777cf191f350b33dd78bdbb3
Mitch Denny <mitch@mitchdenny.com>
2019-01-07 15:53:24 -0800

    Added warm-up code.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Warmup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(args[0]);
+            var files = directory.GetFiles("*.*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    Console.Write($"Reading {file.FullName}...");
+                    var content = reader.ReadToEnd();
+                    Console.WriteLine($"done!");
+                }                
+            }
+        }
+    }
+}
