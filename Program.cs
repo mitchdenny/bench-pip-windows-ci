693a52b8d98dfdb1614e1690b86c90dc87d6c1c9
Mitch Denny <mitch@mitchdenny.com>
2019-01-07 21:14:00 -0800

    Added more selective warm-up logic.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -8,16 +9,36 @@ namespace Warmup
     {
         static void Main(string[] args)
         {
-            var directory = new DirectoryInfo(args[0]);
-            var files = directory.GetFiles("*.*", SearchOption.AllDirectories);
+            if (args.Length % 2 != 0) throw new InvalidOperationException();
 
-            for (var fileIndex = 0; fileIndex < files.Length; fileIndex++)
+            var files = new List<FileInfo>();
+
+            for (var argIndex = 0; argIndex < args.Length; argIndex = argIndex + 2)
+            {
+                if (args[argIndex] == "-d")
+                {
+                    var directory = new DirectoryInfo(args[argIndex + 1]);
+                    var additionalFiles = directory.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+                    files.AddRange(additionalFiles);
+                }
+                else
+                {
+                    var directory = new DirectoryInfo(args[argIndex + 1]);
+                    var additionalFiles = directory.GetFiles("*.*", SearchOption.AllDirectories);
+                    files.AddRange(additionalFiles);
+                }
+            }
+
+            Console.WriteLine($"Starting warm-up of {files.Count} files.");
+            return;
+
+            for (var fileIndex = 0; fileIndex < files.Count; fileIndex++)
             {
                 using (var reader = new StreamReader(files[fileIndex].FullName))
                 {
                     var stopwatch = new Stopwatch();
                     stopwatch.Start();
-                    Console.Write($"Reading {fileIndex} of {files.Length} => {files[fileIndex].FullName}...");
+                    Console.Write($"Reading {fileIndex} of {files.Count} => {files[fileIndex].FullName}...");
                     var content = reader.ReadToEnd();
                     stopwatch.Stop();
                     Console.WriteLine($"done ({stopwatch.ElapsedMilliseconds})!");
